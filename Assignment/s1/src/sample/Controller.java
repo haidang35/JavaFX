77b8488ca96a97ba503d81dbc2daba2e2ec7c45f
Nguyễn Hải Đăng <71062801+nguyenhaidangyq@users.noreply.github.com>
2021-03-22 12:32:29 +0700

    update

---
+++ Assignment/s1/src/sample/Controller.java
@@ -0,0 +1,74 @@
+package sample;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Controller {
+     public TextField name;
+     public TextField age;
+     public TextField mark;
+     public TextArea textForm;
+     public ArrayList<Student> list= new ArrayList<>();
+
+     public static Boolean flag=true;
+     public void submit(){
+        String n=name.getText();
+        String a=age.getText();
+        int ag=Integer.parseInt(a);
+        String m=mark.getText();
+        double ma=Double.parseDouble(m);
+        if(!n.isEmpty() && !a.isEmpty() && !m.isEmpty()){
+            Student std= new Student();
+            std.setName(n);
+            std.setAge(ag);
+            std.setMark(ma);
+            String txt="";
+            list.add(std);
+            for(Student arr: list){
+                txt += "Name: "+arr.getName() + " --- Age: " + arr.getAge() +" --- Mark: " +arr.getMark() + "\r\n";
+            }
+            textForm.setText(txt);
+            name.setText("");
+            age.setText("");
+            mark.setText("");
+        }
+     }
+     public void arrange(){
+         if(flag){
+           Collections.sort(list, new Comparator<Student>() {
+               @Override
+               public int compare(Student o1, Student o2) {
+                   return o2.getMark()-o1.getMark()>0?-1:0;
+               }
+           });
+             String txt="";
+             for(Student arr: list){
+                 txt += "Name: "+arr.getName() + " --- Age: " + arr.getAge() +" --- Mark: " +arr.getMark() + "\r\n";
+             }
+             textForm.setText(txt);
+             flag = false;
+
+
+         }else if(!flag){
+             Collections.sort(list, new Comparator<Student>() {
+                 @Override
+                 public int compare(Student o1, Student o2) {
+                     return o2.getMark()-o1.getMark()<0?-1:0;
+                 }
+             });
+             String txt="";
+             for(Student arr: list){
+                 txt += "Name: "+arr.getName() + " --- Age: " + arr.getAge() +" --- Mark: " +arr.getMark() + "\r\n";
+             }
+             textForm.setText(txt);
+             flag=true;
+         }
+     }
+
+
+}
---
+++ Assignment/s1/src/sample/Student.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.scene.control.TextField;
+
+public class Student {
+    public String name;
+    public int age;
+    public double mark;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+}
