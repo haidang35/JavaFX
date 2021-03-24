87458a03b54b92ece65c2ffa7db274da2fc80752
Nguyễn Hải Đăng <71062801+nguyenhaidangyq@users.noreply.github.com>
2021-03-24 22:41:29 +0700

    Assignment 2

---
+++ Assignment/s2/src/sample/Student.java
@@ -0,0 +1,40 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package sample;
+
+public class Student {
+    public String name;
+    public int age;
+    public double mark;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getMark() {
+        return this.mark;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+}
+
